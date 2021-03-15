94001f601f98830b806cce04fe18fdadd1a2d139
mat <mat.mazurk@gmail.com>
2021-03-15 17:07:35 +0100

    create blank fragments

---
+++ app/src/main/java/com/mat/compass/CompassFragment.kt
@@ -0,0 +1,18 @@
+package com.mat.compass
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CompassFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_compass, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/mat/compass/CoordsInputFragment.kt
@@ -0,0 +1,17 @@
+package com.mat.compass
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CoordsInputFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_coords_input, container, false)
+    }
+}
